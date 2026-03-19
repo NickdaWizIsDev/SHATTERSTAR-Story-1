87705fcb5eb9801ca8ad5be3d704e9f3a872d83f
NickdaWizIsDev <nicocastrito2005@gmail.com>
2026-03-19 16:30:50 -0300

    Jump work.

---
+++ Assets/Scripts/General Use/TouchingDirections.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class TouchingDirections : MonoBehaviour
+{
+    public ContactFilter2D castFilter;
+    public float groundDistance = 0.05f;
+    public float wallDistance = 0.2f;
+
+    [SerializeField] Collider2D touchingCol;
+
+    RaycastHit2D[] groundHits = new RaycastHit2D[5];
+    RaycastHit2D[] wallHits = new RaycastHit2D[5];
+
+    [SerializeField]
+    private bool isGrounded;
+    public bool Ground
+    {
+        get
+        {
+            return isGrounded;
+        }
+        private set
+        {
+            isGrounded = value;
+        }
+    }
+
+    [SerializeField]
+    private bool isOnWall;
+
+    private Vector2 WallCheckDirection => gameObject.transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+    public bool Wall
+    {
+        get
+        {
+            return isOnWall;
+        }
+        private set
+        {
+            isOnWall = value;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Ground = touchingCol.Cast(Vector2.down, castFilter, groundHits, groundDistance) > 0;
+        Wall = touchingCol.Cast(WallCheckDirection, castFilter, wallHits, wallDistance) > 0;
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,14 +5,25 @@ using UnityEngine.InputSystem;
 namespace Player
 {
     public class PlayerMovement : MonoBehaviour
-    {        
-        [SerializeField] internal Vector2 movementVector; 
+    {
+        [Header("Values")]
+        [SerializeField] internal Vector2 movementVector;
+
+        [Header("Horizontal Movement")]
         [SerializeField] internal float runVelocity;
-        [SerializeField][Range(0,1)] internal float stopLerpValue;
+        [SerializeField][Range(0, 1)] internal float stopLerpValue;
+
+        [Header("Vertical Movement")]
         [SerializeField] internal float jumpForce;
+        [SerializeField] internal float coyoteTimeWindow;
+        [SerializeField] internal float jumpBufferTimeWindow;
+        private bool buffering;
+        private float jumpLastPressedTime;
+        private float lastGroundedTime;
 
         [Header("References")]
         [SerializeField] Rigidbody2D body;
+        [SerializeField] TouchingDirections touching;
         public void OnMove(InputValue moveValue)
         {
             movementVector = moveValue.Get<Vector2>();
@@ -20,25 +31,37 @@ namespace Player
 
         void Update()
         {
-            // Acceleration Curve
-
-
+            if (touching.Ground)
+            {
+                if (buffering)
+                {
+                    if (Time.time - jumpLastPressedTime <= jumpBufferTimeWindow)
+                    {
+                        Jump();
+                    }
+                }
+                if (lastGroundedTime != 0) lastGroundedTime = 0;
+            }
+            else if (!touching.Ground && lastGroundedTime == 0)
+            {
+                lastGroundedTime = Time.time;
+            }
         }
 
         internal void Move()
-        {            
+        {
             var vel = Mathf.Abs(body.linearVelocityX);
 
             // Turning logic
-            if(Mathf.Sign(body.linearVelocityX) != Mathf.Sign(movementVector.x) && vel > 0.5f)
+            if (Mathf.Sign(body.linearVelocityX) != Mathf.Sign(movementVector.x) && vel > 0.5f)
             {
                 body.linearVelocityX = Mathf.Lerp(body.linearVelocityX, movementVector.x * runVelocity, stopLerpValue);
                 // runState = state.turning or something of the sort
-                return;      
+                return;
             }
 
             // Acceleration, divided into two sections (acceleration is slower after the velocity is half way to the target)
-            if(vel < runVelocity/2) body.AddForceX(movementVector.x * runVelocity);
+            if (vel < runVelocity / 2) body.AddForceX(movementVector.x * runVelocity);
             else
             {
                 var force = runVelocity - vel;
@@ -52,6 +75,22 @@ namespace Player
 
         public void OnJump(InputValue jumpValue)
         {
+            if (jumpValue.isPressed)
+            {
+                if (touching.Ground)
+                {
+                    Jump();
+                }
+                else
+                {
+                    if (Time.time - lastGroundedTime <= coyoteTimeWindow) Jump();
+                    else jumpLastPressedTime = Time.time;
+                }
+            }
+        }
+
+        private void Jump()
+        {            
             body.AddForceY(jumpForce, ForceMode2D.Impulse);
         }
     }
